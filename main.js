8042b46780a4e2f09d0eb4a526a256b48f59cdd2
Sarah Vessels <cheshire137@gmail.com>
2016-11-16 11:18:01 -0600

    Show total cost of monthly services

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 (function() {
+  function calculateServiceTotal() {
+    const serviceCheckboxes = document.querySelectorAll('input[name="subscription"]:checked')
+    let total = 0
+    for (let i = 0; i < serviceCheckboxes.length; i++) {
+      total = total + parseFloat(serviceCheckboxes[i].value)
+    }
+    return total
+  }
+
   function subscribeToChange(event) {
     const serviceSelection = document.getElementById('service-selection')
     serviceSelection.style.display = 'none'
+
+    const serviceTotal = calculateServiceTotal().toFixed(2)
+    document.getElementById('service-total').textContent = `$${serviceTotal}`
 
     const results = document.getElementById('results')
     results.style.display = 'block'
