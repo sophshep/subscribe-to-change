b922191bba7feffed97f39bda321f3a5ae7717a8
Sarah Vessels <cheshire137@gmail.com>
2016-11-16 11:09:19 -0600

    Trigger page change on button click

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 (function() {
+  function subscribeToChange(event) {
+    const serviceSelection = document.getElementById('service-selection')
+    serviceSelection.style.display = 'none'
+
+    const results = document.getElementById('results')
+    results.style.display = 'block'
+  }
+
+  function hookUpButton() {
+    const button = document.getElementById('subscribe-to-change')
+    button.addEventListener('click', subscribeToChange)
+  }
+
+  hookUpButton()
 })()
