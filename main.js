f692c877c6fd279bd9bfbd5de80cad7f88378ac9
Sarah Vessels <cheshire137@gmail.com>
2016-11-16 11:31:29 -0600

    Enable/disable button when cause and service checked

--- main.js
+++ main.js
@@ -24,5 +24,21 @@
     button.addEventListener('click', subscribeToChange)
   }
 
+  function toggleButton() {
+    const serviceCheckboxes = document.querySelectorAll('input[name="subscription"]:checked')
+    const causeCheckboxes = document.querySelectorAll('inut[name="cause"]:checked')
+    console.log('services checked:', serviceCheckboxes.length, '/ causes checked:', causeCheckboxes.length)
+    const button = document.getElementById('subscribe-to-change')
+    button.disabled = causeCheckboxes.length < 1 || serviceCheckboxes.length < 1
+  }
+
+  function listenForCheckboxChanges() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].addEventListener('change', toggleButton)
+    }
+  }
+
   hookUpButton()
+  listenForCheckboxChanges()
 })()
